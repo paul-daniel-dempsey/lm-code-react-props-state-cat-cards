7fd06146a5b0a9a985344e31ae68e13fb01e162f
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-04-10 17:22:29 +0100

    Act9 Pt2: pet food comma spearated spacing

--- src/components/pet_card.tsx
+++ src/components/pet_card.tsx
@@ -12,12 +12,13 @@ const PetCard : React.FC <petProps> = (props) => {
 
     const {key, type, name , species, favFoods, birthYear } = props.petObject
     const {image, altText,licenceType, licenceUrl,attributionName,attributionUrl } = props.petObjectImg
+    const foods = favFoods.join(", ");
 
     return (
         <div className="card">
         <h3 className="card__text card__header">{type}:{name}</h3>
         <p className="card__text">Species: {species}</p>
-        <p className="card__text">Favourite Food(s): {favFoods}</p>
+        <p className="card__text">Favourite Food(s): {foods}</p>
         <p className="card__text">Birth Year: {birthYear}</p>
         <PetImage image={image} 
                     altText={altText} 
